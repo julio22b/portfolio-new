91c75192de5add6ddbeb61afe5004367c431b98a
julio <jbermudezfc90@gmail.com>
2026-06-10 15:21:49 -0400

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juliobermudez.dev',
-  base:'/',
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    site: 'https://juliobermudez.dev',
+    base: '/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [icon()]
+    integrations: [icon(), sitemap()],
 });
